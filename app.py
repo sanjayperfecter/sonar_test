793f5838c3c54a8f0a5227c8ccdc2049dbe8bce6
Sanjay V <sanjay@perfecter.ai>
2026-02-05 21:26:15 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+"""sdfbd"""
+
+
+from fastapi import APIRouter
+import os  # unused import
+
+router = APIRouter()
+
+@router.get("/healthz")
+def healthz():
+    """dfb"""
+    return {"ok": True}
+return
